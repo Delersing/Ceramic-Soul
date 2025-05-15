5b111520c2f7bc716e7a5e5b0a9590dc8ef9797b
Ivan <imoroz404@gmail.com>
2025-05-15 15:50:29 +0300

    Ready second phase first section

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 // сторонние из npm пакетов, pure.css
-import "purecss/build/grids-min.css";
-import "purecss/build/grids-responsive-min.css";
+// import "purecss/build/grids-min.css"; // мы берем не из Npm пакету уже, а из локальных папок Libs т.к. мы перенесли для нормальной натсройки промежутков media 1024 и 1200. Далее удалили Purecss из проекта за ненадобностью, т.к. перенесли нужные файлы из node modules -> purecss -> dist -> grids-min.css / grids-responsive-min.css которые мы использовали в проекте, и так-же просто подключили их в style.scss
+// import "purecss/build/grids-responsive-min.css";
 // мои файлики
 import "/src/sass/style.scss";
