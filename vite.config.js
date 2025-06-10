7e6c00b60afa246e2c71ba166a9cc9e1c728b047
Ivan <imoroz404@gmail.com>
2025-06-10 15:05:58 +0300

    nothing new?

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { defineConfig } from 'vite'
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
